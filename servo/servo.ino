b8773f87178bef6ec1b7da0b3bc5ffbdb83ca1db
LeonardoNavaC <A01750595@itesm.mx>
2022-11-15 21:18:36 -0600

    updating servomotor script with a stepper motor

--- servo/servo.ino
+++ servo/servo.ino
@@ -3,6 +3,9 @@
 // Declaramos la variable para controlar el servo
 int pushButton = 3;
 bool state = HIGH;
+const int stepPin = 5;
+const int dirPin = 4; 
+int customDelay;
 Servo servoMotor;
  
 void setup() {
@@ -11,13 +14,17 @@ void setup() {
  
   // Iniciamos el servo para que empiece a trabajar con el pin 9
   servoMotor.attach(9);
+  servoMotor.write(90); 
   pinMode(3, INPUT);
+  pinMode(stepPin,OUTPUT); //Nofunciona jeje
+  pinMode(dirPin,OUTPUT);
+  digitalWrite(dirPin,HIGH); 
   
 }
  
 void loop() {
   
-  Serial.println(digitalRead(pushButton));
+  //Serial.println(digitalRead(pushButton));
   
   if (digitalRead(pushButton)){
     // Desplazamos a la posición 90º
@@ -29,10 +36,21 @@ void loop() {
   }
 
   if (state == HIGH){
+    //customDelay = 2500;
+    //digitalWrite(stepPin, HIGH);
+    //delayMicroseconds(customDelay);
+    //digitalWrite(stepPin, LOW);
+    //delayMicroseconds(customDelay); 
     servoMotor.write(90);  
   }
   else if (state == LOW){
-    servoMotor.write(180);  
+    servoMotor.write(180);
+    
   }
-  delay(100);
+  customDelay = 5000;
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(customDelay);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(customDelay);
+
 }
